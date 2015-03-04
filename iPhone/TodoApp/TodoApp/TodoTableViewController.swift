23f49868cae961d7308ed849a9a21a289a32acd6
mitake <mitake.ck@gmail.com>
2015-03-04 20:06:52 +0900

    tableview に cell をとりあえず追加

--- iPhone/TodoApp/TodoApp/TodoTableViewController.swift
+++ iPhone/TodoApp/TodoApp/TodoTableViewController.swift
@@ -9,12 +9,38 @@
 import Foundation
 import UIkit
 
-class TodoTableViewController : UIViewController{
+class TodoTableViewController : UIViewController, UITableViewDataSource{
+    
+    var tableView : UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let header = UIImageView(frame: CGRect(x: 0, y: 0, width: 320, height: 64))
+        header.image = UIImage(named: "header")
+        self.view.addSubview(header)
+        
         let title = UILabel(frame: CGRect(x: 10, y: 20, width: 310, height: 44))
         title.text = "Hello Swift"
-        self.view.addSubview(title)
+        header.addSubview(title)
+        
+        let screenWidth = UIScreen.mainScreen().bounds.size.height
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 60, width: 320, height: screenWidth - 60))
+        self.tableView!.dataSource = self
+        
+        self.view.addSubview(self.tableView!)
+        self.view.addSubview(header)
+    }
+    
+    // テーブルの行数を返却する関数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 10
+    }
+    
+    // テーブルで表示するセルを生成して返却する関数
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel!.text = "todo"
+        return cell
     }
 }
