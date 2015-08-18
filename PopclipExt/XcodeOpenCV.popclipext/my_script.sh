3251314b568889cc092ef4043c43b6d3a54ee40f
mitake <mitake.ck@gmail.com>
2015-08-18 18:22:03 +0900

    XCode で OpenCV の設定するとき other_linker_flags を一斉に設定できる Popclip 拡張作成

---
+++ PopclipExt/XcodeOpenCV.popclipext/my_script.sh
@@ -0,0 +1,19 @@
+other_linker_flags='-lopencv_videostab\n
+-lopencv_videoio\n
+-lopencv_video\n
+-lopencv_ts\n
+-lopencv_superres\n
+-lopencv_stitching\n
+-lopencv_shape\n
+-lopencv_photo\n
+-lopencv_objdetect\n
+-lopencv_ml\n
+-lopencv_imgproc\n
+-lopencv_imgcodecs\n
+-lopencv_highgui\n
+-lopencv_flann\n
+-lopencv_features2d\n
+-lopencv_core\n
+-lopencv_calib3d'
+
+echo $other_linker_flags
